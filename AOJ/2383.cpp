fa0c263fe380ee0a1ba72160f0d9229c961b9e12
makecir <konpeist@yahoo.co.jp>
2018-10-29 20:22:39 +0900

    first commit

---
+++ AOJ/2383.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using vs = vector<string>;
+using msi = map<string, int>;
+using mii = map<int, int>;
+using psi = pair<string, int>;
+using pii = pair<int, int>;
+using vlai = valarray<int>;
+#define rep(i,n) for(int i=0;i<n;i++)
+#define range(i,s,n) for(int i=s;i<n;i++)
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define fs first
+#define sc second
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define INF 1E9
+#define LINF 1E18*5
+#define EPS 1E-9
+#define MOD 1000000007
+#define PI 3.1415926535897932384
+
+template<class S, class T>ostream& operator<<(ostream&os, pair<S, T>p) { os << "[" << p.first << ", " << p.second << "]"; return os; };
+template<class S>auto&operator<<(ostream&os, vector<S>t) { bool a = 1; for (auto s : t) { os << (a ? "" : " ") << s; a = 0; } return os; }
+
+//int dx[]={1,1,1,0,-1,-1,-1,0},dy[8]={1,0,-1,-1,-1,0,1,1,1};
+//
+constexpr ll gcd(ll a, ll b) { return b ? gcd(b, a%b) : a; }
+
+class compare {
+public:
+	bool operator()(tuple<int, int> a, tuple<int, int> b) {
+		return (get<1>(a) < get<1>(b));
+	}
+
+};
+
+int main() {
+	int n, t, in;
+	ll  ans = 1;
+	cin >> n >> t;
+	vi v(n);
+	vi vsum(100001, 0);
+	rep(i, n) {
+		cin >> v[i];
+		vsum[v[i]]++;
+	}
+	sort(all(v));
+	range(i, 1, vsum.size()) {
+		vsum[i] += vsum[i - 1];
+	}
+	rep(i, n) {
+		ans *= (i + 1 - vsum[max(v[i] - t-1, 0)]);
+		ans %= MOD; 
+	}
+	cout << ans << endl;
+}
