3d172d063af5b7b479799cb6d3e66c395b3f5108
makecir <konpeist@yahoo.co.jp>
2019-01-25 23:20:28 +0900

    20190125

--- Library/func.h
+++ Library/func.h
@@ -1,1 +1,13 @@
 constexpr ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll exgcd(ll a,ll b,ll &x,ll &y) {
+	if(b==0){
+		x=1;
+		y=0;
+		return a;
+	}
+	ll d=exgcd(b,a%b,y,x);
+	y-=a/b*x;
+	return d;
+}
+
+
