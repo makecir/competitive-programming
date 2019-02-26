a27d54d30855e821edbfc604bfed762e4d38e60b
makecir <konpeist@yahoo.co.jp>
2019-02-27 04:00:07 +0900

    20190227

---
+++ AtCoder/abc117/d.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using vb=vector<bool>;
+using vvb=vector<vb>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+using tll=tuple<ll,ll>;
+using tlll=tuple<ll,ll,ll>;
+using vs=vector<string>;
+
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define rep(i,n) range(i,0,n)
+#define rrep(i,n) for(int i=(n)-1;i>=0;i--)
+#define range(i,a,n) for(int i=(a);i<(n);i++)
+
+#define LINF ((ll)1ll<<60)
+#define INF ((int)1<<30)
+#define EPS (1e-9)
+#define MOD (1000000007ll)
+#define fcout(a) cout<<setprecision(a)<<fixed
+#define fs first
+#define sc second
+#define PI 3.1415926535897932384
+
+int dx[]={1,0,-1,0,1,-1,-1,1},dy[]={0,1,0,-1,1,1,-1,-1};
+
+template<class T>bool chmax(T&a,T b){if(a<b){a=b; return true;}return false;}
+template<class T>bool chmin(T&a,T b){if(a>b){a=b; return true;}return false;}
+template<class S>S acm(vector<S>&a){return accumulate(all(a),S());}
+template<class S>S max(vector<S>&a){return *max_element(all(a));}
+template<class S>S min(vector<S>&a){return *min_element(all(a));}
+void YN(bool b){cout<<(b?"YES":"NO")<<"\n";}
+void Yn(bool b){cout<<(b?"Yes":"No")<<"\n";}
+void yn(bool b){cout<<(b?"yes":"no")<<"\n";}
+
+ll max(int a,ll b){return max((ll)a,b);} ll max(ll a,int b){return max(a,(ll)b);}
+template<class T>void puta(T&&t){cout<<t<<"\n";}
+template<class H,class...T>void puta(H&&h,T&&...t){cout<<h<<' ';puta(t...);}
+template<class S,class T>ostream&operator<<(ostream&os,pair<S,T>p){os<<"["<<p.first<<", "<<p.second<<"]";return os;};
+template<class S>auto&operator<<(ostream&os,vector<S>t){bool a=1; for(auto s:t){os<<(a?"":" ")<<s;a=0;} return os;}
+#define _cTime (chrono::system_clock::now())
+#define progress (chrono::duration_cast<chrono::milliseconds>(_cTime-_sTime).count())
+#define reset _sTime=_cTime
+auto reset;
+
+ll dp[60][2];
+
+int main(){
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	ll n,k;
+	cin>>n>>k;
+	vl a(n);
+	vvl dp(60,vl(2,-1));
+	rep(i,60)dp[i][0]=0;
+	vb b(60);
+	rep(i,n)cin>>a[i];
+	bool fl=false;
+	rrep(i,59){
+		ll m=(1ll<<i);
+		ll cnt=0;
+		rep(j,n)if(a[j]&m)cnt++;
+		if(dp[i+1][0]>=0){
+			if(m&k){
+				chmax(dp[i][0],dp[i+1][0]+m*(n-cnt));
+				chmax(dp[i][1],dp[i+1][0]+m*cnt);
+			}
+			else chmax(dp[i][0],dp[i+1][0]+m*cnt);
+		}
+		if(dp[i+1][1]>=0)chmax(dp[i][1],dp[i+1][1]+m*max(cnt,n-cnt));
+	}
+	cout<<max(dp[0][0],dp[0][1])<<endl;
+}
