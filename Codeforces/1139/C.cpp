0c7ceec7a4e38e9a97f8e07b105223e03a156895
makecir <konpeist@yahoo.co.jp>
2019-03-23 05:18:15 +0900

    20190323

---
+++ Codeforces/1139/C.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using vb=vector<bool>;
+using vvb=vector<vb>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+using tll=tuple<ll,ll>;
+using tlll=tuple<ll,ll,ll>;
+using vs=vector<string>;
+
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define rep(i,n) range(i,0,n)
+#define rrep(i,n) for(int i=(n)-1;i>=0;i--)
+#define range(i,a,n) for(int i=(a);i<(n);i++)
+
+#define LINF ((ll)1ll<<60)
+#define INF ((int)1<<30)
+#define EPS (1e-9)
+#define MOD (1000000007ll)
+#define fcout(a) cout<<setprecision(a)<<fixed
+#define fs first
+#define sc second
+#define PI 3.1415926535897932384
+
+int dx[]={1,0,-1,0,1,-1,-1,1},dy[]={0,1,0,-1,1,1,-1,-1};
+
+template<class T>bool chmax(T&a,T b){if(a<b){a=b; return true;}return false;}
+template<class T>bool chmin(T&a,T b){if(a>b){a=b; return true;}return false;}
+template<class S>S acm(vector<S>&a){return accumulate(all(a),S());}
+template<class S>S max(vector<S>&a){return *max_element(all(a));}
+template<class S>S min(vector<S>&a){return *min_element(all(a));}
+void YN(bool b){cout<<(b?"YES":"NO")<<"\n";}
+void Yn(bool b){cout<<(b?"Yes":"No")<<"\n";}
+void yn(bool b){cout<<(b?"yes":"no")<<"\n";}
+
+ll max(int a,ll b){return max((ll)a,b);} ll max(ll a,int b){return max(a,(ll)b);}
+template<class T>void puta(T&&t){cout<<t<<"\n";}
+template<class H,class...T>void puta(H&&h,T&&...t){cout<<h<<' ';puta(t...);}
+template<class S,class T>ostream&operator<<(ostream&os,pair<S,T>p){os<<"["<<p.first<<", "<<p.second<<"]";return os;};
+template<class S>auto&operator<<(ostream&os,vector<S>t){bool a=1; for(auto s:t){os<<(a?"":" ")<<s;a=0;} return os;}
+#define _cTime (chrono::system_clock::now())
+#define progress (chrono::duration_cast<chrono::milliseconds>(_cTime-_sTime).count())
+#define reset _sTime=_cTime
+auto reset;
+
+struct UnionFind{
+    vl par,dist;
+    UnionFind(int x){par.assign(x,-1); dist.assign(x,0);}
+    ll find(ll x){return par[x]<0?x:find(par[x]);}
+    ll depth(ll x){return par[x]<0?0:depth(par[x])+dist[x];}
+    bool same(ll x,ll y){return find(x)==find(y);}
+    ll size(ll x){return -par[find(x)];}
+    ll diff(ll x,ll y){return same(x,y)?depth(x)-depth(y):LINF;}
+    void unite(ll x,ll y,ll k=0){
+        k+=depth(y); k-=depth(x); k=-k;
+        x=find(x); y=find(y);
+        if(x==y)return;
+        if(size(x)<size(y)){swap(x,y);k=-k;}
+        par[x]+=par[y]; par[y]=x; dist[y]=k;
+    }
+};
+
+int main(){
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	ll n,k,u,v,x,ans=1;
+	cin>>n>>k;
+	UnionFind U(n+1);
+	rep(i,n-1){
+		cin>>u>>v>>x;
+		if(x==0)U.unite(u,v);
+	}
+	rep(i,k)ans*=n,ans%=MOD;
+	range(i,1,n+1){
+		if(U.same(0,i))continue;
+		ll sub=1;
+		if(U.size(i)!=1){
+			rep(j,k)sub*=U.size(i),sub%=MOD;
+		}
+		ans=(ans-sub+MOD)%MOD;
+		U.unite(0,i);
+	}
+	cout<<ans<<endl;
+}
