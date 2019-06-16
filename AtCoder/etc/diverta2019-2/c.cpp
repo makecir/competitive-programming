a47d23522c8dd350bd6414bbe68e418a87704c83
makecir <konpeist@gmail.com>
2019-06-17 07:41:02 +0900

    ad

---
+++ AtCoder/etc/diverta2019-2/c.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using vb=vector<bool>;
+using vvb=vector<vb>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+using tll=tuple<ll,ll>;
+using tlll=tuple<ll,ll,ll>;
+using vs=vector<string>;
+
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define rep(i,n) range(i,0,n)
+#define rrep(i,n) for(int i=(n)-1;i>=0;i--)
+#define range(i,a,n) for(int i=(a);i<(n);i++)
+
+#define LINF ((ll)1ll<<60)
+#define INF ((int)1<<30)
+#define EPS (1e-9)
+#define MOD (1000000007ll)
+#define fcout(a) cout<<setprecision(a)<<fixed
+#define fs first
+#define sc second
+#define PI 3.1415926535897932384
+
+int dx[]={1,0,-1,0,1,-1,-1,1},dy[]={0,1,0,-1,1,1,-1,-1};
+
+template<class T>bool chmax(T&a,T b){if(a<b){a=b; return true;}return false;}
+template<class T>bool chmin(T&a,T b){if(a>b){a=b; return true;}return false;}
+template<class S>S acm(vector<S>&a){return accumulate(all(a),S());}
+template<class S>S max(vector<S>&a){return *max_element(all(a));}
+template<class S>S min(vector<S>&a){return *min_element(all(a));}
+void YN(bool b){cout<<(b?"YES":"NO")<<"\n";}
+void Yn(bool b){cout<<(b?"Yes":"No")<<"\n";}
+void yn(bool b){cout<<(b?"yes":"no")<<"\n";}
+
+ll max(int a,ll b){return max((ll)a,b);} ll max(ll a,int b){return max(a,(ll)b);}
+template<class T>void puta(T&&t){cout<<t<<"\n";}
+template<class H,class...T>void puta(H&&h,T&&...t){cout<<h<<' ';puta(t...);}
+template<class S,class T>ostream&operator<<(ostream&os,pair<S,T>p){os<<"["<<p.first<<", "<<p.second<<"]";return os;};
+template<class S>auto&operator<<(ostream&os,vector<S>t){bool a=1; for(auto s:t){os<<(a?"":" ")<<s;a=0;} return os;}
+
+int main(){
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	ll n,next;
+	cin>>n;
+	vl v(n),x(n-1),y(n-1);
+	bool pl=false,mi=false;
+	rep(i,n)cin>>v[i],pl|=(v[i]>=0),mi|=(v[i]<=0);
+	if(!pl|!mi){
+		sort(all(v),[](auto &a,auto &b){
+		return (abs(a)<abs(b));
+		});
+		swap(v[0],v[1]);
+		next=v[0];
+		rep(i,n-1){
+			if(i!=0){
+				if(v[i+1]>0&&next>0)next*=-1;
+				else if(v[i+1]<0&&next<0)next*=-1;
+			}
+			x[i]=next;
+			y[i]=v[i+1];
+			next=abs(next-v[i+1]);
+		}
+	}
+	else{
+		rep(i,n-1){
+			if(v[0]>=0&&v[i+1]<=0||v[0]<=0&&v[i+1]>=0)swap(v[1],v[i+1]);
+		}
+		next=v[0];
+		rep(i,n-1){
+			if(i!=0){
+				if(v[i+1]>0&&next>0)next*=-1;
+				else if(v[i+1]<0&&next<0)next*=-1;
+			}
+			x[i]=next;
+			y[i]=v[i+1];
+			next=abs(next-v[i+1]);
+		}
+	}
+	rep(i,n-2){
+		if(x[i+1]<0&&x[i]>y[i])swap(x[i],y[i]);
+		else if(x[i+1]>0&&x[i]<y[i])swap(x[i],y[i]);
+	}
+	if(x[n-2]<y[n-2])swap(x[n-2],y[n-2]);
+	puta(next);
+	rep(i,n-1){
+		puta(x[i],y[i]);
+	}
+}
