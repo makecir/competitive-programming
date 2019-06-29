f0d95081830921d2181fcb7eb3fc6a121e473281
Kakeru Ito <konpeist@gmail.com>
2019-06-29 23:05:41 +0900

    ad

---
+++ AtCoder/abc/abc132/e.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using vb=vector<bool>;
+using vvb=vector<vb>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+using tll=tuple<ll,ll>;
+using tlll=tuple<ll,ll,ll>;
+using vs=vector<string>;
+
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define rep(i,n) range(i,0,n)
+#define rrep(i,n) for(int i=(n)-1;i>=0;i--)
+#define range(i,a,n) for(int i=(a);i<(n);i++)
+
+#define LINF ((ll)1ll<<60)
+#define INF ((int)1<<30)
+#define EPS (1e-9)
+#define MOD (1000000007ll)
+#define fcout(a) cout<<setprecision(a)<<fixed
+#define fs first
+#define sc second
+#define PI 3.1415926535897932384
+
+int dx[]={1,0,-1,0,1,-1,-1,1},dy[]={0,1,0,-1,1,1,-1,-1};
+
+template<class T>bool chmax(T&a,T b){if(a<b){a=b; return true;}return false;}
+template<class T>bool chmin(T&a,T b){if(a>b){a=b; return true;}return false;}
+template<class S>S acm(vector<S>&a){return accumulate(all(a),S());}
+template<class S>S max(vector<S>&a){return *max_element(all(a));}
+template<class S>S min(vector<S>&a){return *min_element(all(a));}
+void YN(bool b){cout<<(b?"YES":"NO")<<"\n";}
+void Yn(bool b){cout<<(b?"Yes":"No")<<"\n";}
+void yn(bool b){cout<<(b?"yes":"no")<<"\n";}
+
+ll max(int a,ll b){return max((ll)a,b);} ll max(ll a,int b){return max(a,(ll)b);}
+template<class T>void puta(T&&t){cout<<t<<"\n";}
+template<class H,class...T>void puta(H&&h,T&&...t){cout<<h<<' ';puta(t...);}
+template<class S,class T>ostream&operator<<(ostream&os,pair<S,T>p){os<<"["<<p.first<<", "<<p.second<<"]";return os;};
+template<class S>auto&operator<<(ostream&os,vector<S>t){bool a=1; for(auto s:t){os<<(a?"":" ")<<s;a=0;} return os;}
+
+template<class T=ll>struct Graph{
+	int n;
+	vector<vector<tuple<ll,T>>>edge;
+	Graph(int N=1):n(N){edge.resize(n);}
+	void add(ll f,ll t,T c,bool d=false){
+		edge[f].emplace_back(t,c);
+		if(!d)edge[t].emplace_back(f,c);
+	}
+	void view(){
+		rep(i,n)for(auto&e:edge[i])
+			puta(i,"=>",get<0>(e),", cost :",get<1>(e));
+	}
+};
+
+template<class T=ll>struct Dijkstra:Graph<T>{
+	using Graph<T>::Graph;
+	Dijkstra(int N=1):Graph<T>::Graph(N){}
+	const T mval=numeric_limits<T>::max()/2;
+	vector<T>dist(ll s){
+		vector<T> ret(this->n,mval);
+		priority_queue<tuple<T,ll>> q;
+		q.emplace(T(),s);
+		while(!q.empty()){
+			T c; ll p; tie(c,p)=q.top(); q.pop();
+			if(ret[p]!=mval)continue;
+			ret[p]=c=-c;
+			for(auto&e:this->edge[p]){
+				ll nxt;T cost;
+				tie(nxt,cost)=e;
+				if(ret[nxt]<=ret[p]+cost)continue;
+				q.emplace(-ret[p]-cost,nxt);
+			}
+		}
+		return ret;
+	}
+	T dist(ll s,ll t){return dist(s)[t];}
+};
+
+int main(){
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	ll n,m;
+	cin>>n>>m;
+	Dijkstra<ll> g(n*3);
+	int u,v,s,t;
+	rep(i,m){
+		cin>>u>>v;u--;v--;
+		rep(j,3)g.add(u+n*(j),v+n*((j+1)%3),1,true);
+	}
+	cin>>s>>t;
+	ll ans=g.dist(s-1,t-1);
+	cout<<(ans%3==0&&ans!=4611686018427387903?ans/3:-1)<<endl;
+
+}
