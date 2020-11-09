ff9af9b7565399124604f9e0747d629a0ddbfa31
makecir <konpeist@yahoo.co.jp>
2020-11-09 23:39:40 +0900

    Add: abcなど

---
+++ AtCoder/abc/abc177/f.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll=long long;
+using vb=vector<bool>;
+using vvb=vector<vb>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+using tll=tuple<ll,ll>;
+using tlll=tuple<ll,ll,ll>;
+using vs=vector<string>;
+
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define rep(i,n) range(i,0,n)
+#define rrep(i,n) for(ll i=(n)-1;i>=0;i--)
+#define range(i,a,n) for(ll i=(a);i<(n);i++)
+
+#define LINF ((ll)1ll<<60)
+#define INF ((int)1<<30)
+#define EPS (1e-9)
+#define MOD (1000000007ll)
+#define fcout(a) cout<<setprecision(a)<<fixed
+#define fs first
+#define sc second
+#define PI 3.1415926535897932384
+
+int dx[]={1,0,-1,0,1,-1,-1,1},dy[]={0,1,0,-1,1,1,-1,-1};
+
+template<class T>bool chmax(T&a,T b){if(a<b){a=b; return true;}return false;}
+template<class T>bool chmin(T&a,T b){if(a>b){a=b; return true;}return false;}
+template<class S>S acm(vector<S>&a){return accumulate(all(a),S());}
+template<class S>S max(vector<S>&a){return *max_element(all(a));}
+template<class S>S min(vector<S>&a){return *min_element(all(a));}
+void YN(bool b){cout<<(b?"YES":"NO")<<"\n";}
+void Yn(bool b){cout<<(b?"Yes":"No")<<"\n";}
+void yn(bool b){cout<<(b?"yes":"no")<<"\n";}
+int sgn(const double&r){return (r>EPS)-(r<-EPS);} // a>0  : sgn(a)>0
+int sgn(const double&a,const double&b){return sgn(a-b);} // b<=c : sgn(b,c)<=0
+
+ll max(int a,ll b){return max((ll)a,b);} ll max(ll a,int b){return max(a,(ll)b);}
+template<class T>void puta(T&&t){cout<<t<<"\n";}
+template<class H,class...T>void puta(H&&h,T&&...t){cout<<h<<' ';puta(t...);}
+template<class S,class T>ostream&operator<<(ostream&os,pair<S,T>p){os<<"["<<p.first<<", "<<p.second<<"]";return os;}
+template<class S>auto&operator<<(ostream&os,vector<S>t){bool a=1; for(auto s:t){os<<(a?"":" ")<<s;a=0;} return os;}
+
+int main(){
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	ll h,w,a,b;
+	cin>>h>>w;
+	set<pll> st;
+	multiset<ll> ms;
+	rep(i,w)st.insert(pll(i,i)),ms.insert(0);
+	ms.insert(LINF);
+	st.insert(pll(LINF,LINF));
+	rep(i,h){
+		cin>>a>>b;
+		a--;b--;
+		while(1){
+			auto itr=st.lower_bound(pll(a,0));
+			if((*itr).fs>b)break;
+			ms.erase(ms.find((*itr).fs-(*itr).sc));
+			if((*next(itr)).fs>b&&b+1<w){
+				ms.insert(b+1-(*itr).sc);
+				st.insert(pll(b+1,(*itr).sc));
+			}
+			st.erase(itr);
+		}
+		ll ans=*ms.begin()+i+1;
+		if(ans>h+w)ans=-1;
+		puta(ans);
+	}
+}
