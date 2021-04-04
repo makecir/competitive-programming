4bea6ddbc8cf38c090353668786b2dbfc3e837c8
makecir <konpeist@yahoo.co.jp>
2021-04-05 00:35:54 +0900

    Add: 期待値DPの練習

---
+++ AtCoder/arc/arc011/2.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll=long long;
+using vb=vector<bool>;
+using vvb=vector<vb>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+using tll=tuple<ll,ll>;
+using tlll=tuple<ll,ll,ll>;
+using vs=vector<string>;
+
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define rep(i,n) range(i,0,n)
+#define rrep(i,n) for(ll i=(n)-1;i>=0;i--)
+#define range(i,a,n) for(ll i=(a);i<(n);i++)
+#define sz(x) (int)(x).size()
+
+#define LINF ((ll)1ll<<60)
+#define INF ((int)1<<30)
+#define EPS (1e-9)
+#define MOD (1000000007ll)
+//#define MOD (998244353ll)
+#define fcout(a) cout<<setprecision(a)<<fixed
+#define fs first
+#define sc second
+#define PI 3.1415926535897932384
+
+int dx[]={1,0,-1,0,1,-1,-1,1},dy[]={0,1,0,-1,1,1,-1,-1};
+
+template<class T>bool chmax(T&a,T b){if(a<b){a=b; return true;}return false;}
+template<class T>bool chmin(T&a,T b){if(a>b){a=b; return true;}return false;}
+template<class S>S acm(vector<S>&a){return accumulate(all(a),S());}
+template<class S>S max(vector<S>&a){return *max_element(all(a));}
+template<class S>S min(vector<S>&a){return *min_element(all(a));}
+void YN(bool b){cout<<(b?"YES":"NO")<<"\n";}
+void Yn(bool b){cout<<(b?"Yes":"No")<<"\n";}
+void yn(bool b){cout<<(b?"yes":"no")<<"\n";}
+int sgn(const double&r){return (r>EPS)-(r<-EPS);} // a>0  : sgn(a)>0
+int sgn(const double&a,const double&b){return sgn(a-b);} // b<=c : sgn(b,c)<=0
+int popcnt(int x){return __builtin_popcount(x);}
+int popcnt(ll x){return __builtin_popcountll(x);}
+
+ll max(int a,ll b){return max((ll)a,b);} ll max(ll a,int b){return max(a,(ll)b);}
+template<class T>void puta(T&&t){cout<<t<<"\n";}
+template<class H,class...T>void puta(H&&h,T&&...t){cout<<h<<' ';puta(t...);}
+template<class S,class T>ostream&operator<<(ostream&os,pair<S,T>p){os<<"["<<p.first<<", "<<p.second<<"]";return os;}
+template<class S>auto&operator<<(ostream&os,vector<S>t){bool a=1; for(auto s:t){os<<(a?"":" ")<<s;a=0;} return os;}
+
+int main(){
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	ll n;
+	cin>>n;
+	bool fl=false;
+	map<char,char> mp;
+	mp['b']='1';
+	mp['c']='1';
+	mp['t']='3';
+	mp['j']='3';
+	mp['l']='5';
+	mp['v']='5';
+	mp['p']='7';
+	mp['m']='7';
+	mp['n']='9';
+	mp['g']='9';
+	mp['d']='2';
+	mp['w']='2';
+	mp['f']='4';
+	mp['q']='4';
+	mp['s']='6';
+	mp['x']='6';
+	mp['h']='8';
+	mp['k']='8';
+	mp['z']='0';
+	mp['r']='0';
+	rep(i,n){
+		string s,ans;
+		cin>>s;
+		for(auto x:s){
+			if('A'<=x&&x<='Z')x-=('A'-'a');
+			if(mp.find(x)!=mp.end())ans+=mp[x];
+		}
+		if(ans!=""){
+			if(fl)cout<<" ";
+			cout<<ans;
+			fl=true;
+		}
+	}
+	cout<<endl;
+}
---
+++ AtCoder/arc/arc014/4.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll=long long;
+using ld=long double;
+using vb=vector<bool>;
+using vvb=vector<vb>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+using vpll=vector<pll>;
+using tll=tuple<ll,ll>;
+using tlll=tuple<ll,ll,ll>;
+using vs=vector<string>;
+
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define rep(i,n) range(i,0,n)
+#define rrep(i,n) for(ll i=(n)-1;i>=0;i--)
+#define range(i,a,n) for(ll i=(a);i<(n);i++)
+#define sz(x) (int)(x).size()
+
+#define LINF ((ll)1ll<<60)
+#define INF ((int)1<<30)
+#define EPS (1e-9)
+#define MOD (1000000007ll)
+//#define MOD (998244353ll)
+#define fcout(a) cout<<setprecision(a)<<fixed
+#define fs first
+#define sc second
+#define PI 3.1415926535897932384
+
+int dx[]={1,0,-1,0,1,-1,-1,1},dy[]={0,1,0,-1,1,1,-1,-1};
+
+template<class T>bool chmax(T&a,T b){if(a<b){a=b; return true;}return false;}
+template<class T>bool chmin(T&a,T b){if(a>b){a=b; return true;}return false;}
+template<class S>S acm(vector<S>&a){return accumulate(all(a),S());}
+template<class S>S max(vector<S>&a){return *max_element(all(a));}
+template<class S>S min(vector<S>&a){return *min_element(all(a));}
+void YN(bool b){cout<<(b?"YES":"NO")<<"\n";}
+void Yn(bool b){cout<<(b?"Yes":"No")<<"\n";}
+void yn(bool b){cout<<(b?"yes":"no")<<"\n";}
+int sgn(const double&r){return (r>EPS)-(r<-EPS);} // a>0  : sgn(a)>0
+int sgn(const double&a,const double&b){return sgn(a-b);} // b<=c : sgn(b,c)<=0
+int popcnt(int x){return __builtin_popcount(x);}
+int popcnt(ll x){return __builtin_popcountll(x);}
+
+ll max(int a,ll b){return max((ll)a,b);} ll max(ll a,int b){return max(a,(ll)b);}
+template<class T>void puta(T&&t){cout<<t<<"\n";}
+template<class H,class...T>void puta(H&&h,T&&...t){cout<<h<<' ';puta(t...);}
+template<class S,class T>ostream&operator<<(ostream&os,pair<S,T>p){os<<"["<<p.first<<", "<<p.second<<"]";return os;}
+template<class S>auto&operator<<(ostream&os,vector<S>t){bool a=1; for(auto s:t){os<<(a?"":" ")<<s;a=0;} return os;}
+
+int main(){
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	ll all,n,m;
+	cin>>all>>n>>m;
+	vl v(n),sum(n),diff(n-1);
+	rep(i,n)cin>>v[i];
+	rep(i,n-1)diff[i]=v[i+1]-v[i]-1;
+	sort(all(diff));
+	rep(i,n-1)sum[i+1]=sum[i]+diff[i];
+	rep(_,m){
+		ll x,y,ans=n;
+		cin>>x>>y;
+		auto pos=upper_bound(all(diff),x+y)-diff.begin();
+		ans+=sum[pos];
+		ans+=(n-1-pos)*(x+y);
+		ans+=min(v[0]-1,x);
+		ans+=min(all-v.back(),y);
+		puta(ans);
+	}
+}
